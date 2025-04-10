65a22738bd5ca7fe96a8b31922c409711ae8b981
TheLordGibril <gabidule03@gmail.com>
2025-04-10 15:08:45 +0200

    Gérer une infinité de nombres

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 export function add(numbers) {
     if (numbers === '') {
-        return 0;
+        return 0
     }
-    const nums = numbers.split(',');
-    if (nums.length > 2) {
-        throw new Error('Only up to 2 numbers are allowed');
-    }
-    return nums.reduce((acc, num) => acc + parseInt(num), 0);
+    return numbers.split(',').reduce((acc, num) => acc + parseInt(num), 0)
 }
--- main.test.js
+++ main.test.js
@@ -3,18 +3,24 @@ import { add } from './main'
 
 describe('add', () => {
     it('should return 0 if the input is an empty string', () => {
-        expect(add("")).toBe(0);
+        expect(add("")).toBe(0)
     })
 
     it('should return the number itself if the input contains one number', () => {
-        expect(add("5")).toBe(5);
+        expect(add("5")).toBe(5)
     })
 
     it('should return the sum of two numbers separated by a comma', () => {
-        expect(add("1,2")).toBe(3);
+        expect(add("1,2")).toBe(3)
     })
 
-    it('should throw an error if more than 2 numbers are provided', () => {
-        expect(() => add("1,2,3")).toThrow('Only up to 2 numbers are allowed');
+    it('should return the sum of multiple numbers separated by commas', () => {
+        expect(add("1,2,3,4,5")).toBe(15)
+    })
+
+    it('should handle large numbers of inputs correctly', () => {
+        const input = Array.from({ length: 100 }, (_, i) => i + 1).join(',')
+        const expectedSum = (100 * (100 + 1)) / 2 // Sum of first 100 natural numbers
+        expect(add(input)).toBe(expectedSum)
     })
 })
